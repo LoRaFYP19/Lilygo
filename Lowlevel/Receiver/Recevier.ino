9e802edd587fa63511d690c66f3d482b9fc7ab25
Nuwan Udara <nuwanudara36@gmail.com>
2024-01-20 10:33:28 +0530

    updated

--- Lowlevel/Receiver/Recevier.ino
+++ Lowlevel/Receiver/Recevier.ino
@@ -7,10 +7,31 @@ SX1276 radio = new Module(RADIO_CS_PIN, RADIO_DIO0_PIN, RADIO_RST_PIN, RADIO_BUS
 
 #define LoRa_frequency 923.0
 // flag to indicate that a packet was received
+
+const char* ssid = "F1";
+const char* password = "123456789";
+
 volatile bool receivedFlag = false;
 
 // disable interrupt when it's not needed
 volatile bool enableInterrupt = true;
+
+//Your Domain name with URL path or IP address with path
+String serverName = "https://script.google.com/macros/s/AKfycby1U29__LQSKpwApoWNd__NgkccmfbXG_3M7x0h7sirvTr-11KwPZMqW-rmLzdkymFgkA/exec";
+
+bool txSheet = false;
+int64_t rxNumber;
+
+int64_t maxRSSI=-1000;
+int64_t minRSSI=1000;
+int64_t totalTimeOnAir;
+int64_t minTimeOnAir=100000;
+int64_t maxTimeOnAir;
+int64_t tolRSSI;
+int64_t tolrxSize;
+int64_t rxMili,nowMili,defMili;
+int64_t tmiliMin,tmiliH;
+int64_t rssi,rxSize;
 
 // this function is called when a complete packet
 // is received by the module
@@ -29,9 +50,30 @@ void setFlag(void)
 
 void setup()
 {
+    Serial.begin(115200);
     initBoard();
     // When the power is turned on, a delay is required.
     delay(1500);
+        WiFi.begin(ssid, password);
+    Serial.println("Connecting");
+    while(WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    setInterval(60);
+    waitForSync();
+    String val_1 = UTC.dateTime("l, d-M-y H:i:s.v T");
+    // int sec_1 = (val_1.substring(24,27)).toInt();
+    // int msec_1 = (val_1.substring(27,30)).toInt();
+    Serial.println(val_1);
+    txNumber=0;
+    rssi=0;
+
 
     // initialize SX1276 with default settings
     Serial.print(F("Initializing ... "));
@@ -88,6 +130,59 @@ void setup()
     // radio.readData();
     // radio.scanChannel();
 }
+
+void sendDataToSheet(){
+   if(WiFi.status()== WL_CONNECTED){
+     HTTPClient http;
+// var SF = Number(e.parameter.SF);
+// var CR = Number(e.parameter.CR);
+// var txP= Number(e.parameter.txP);
+// var tolRxNum = Number(e.parameter.tolRxNum);
+// var avgRssi = Number(e.parameter.avgRssi);
+// var maxRssi = Number(e.parameter.maxRssi);
+// var minRssi = Number(e.parameter.minRssi);
+// var avgtoa = Number(e.parameter.avgtoa);
+// var minton = Number(e.parameter.minton);
+// var maxton = Number(e.parameter.maxton);
+
+     String serverPath = serverName + "?SF=" + String(LORA_SPREADING_FACTOR) + "&CR=" + String(LORA_CODINGRATE) + "&txP=" + String(TX_OUTPUT_POWER) + "&tolRxNum=" + String(rxNumber) + "&avgRssi=" + String(tolRSSI/rxNumber) + "&maxRssi=" + String(maxRSSI) + "&minRssi=" + String(minRSSI) + "&avgtoa=" + String(totalTimeOnAir/rxNumber) + "&minton=" + String(minTimeOnAir) + "&maxton=" + String(maxTimeOnAir);
+     Serial.println("Total RSSI: " + String(tolRSSI) + " Total Rx Number: " + String(rxNumber) + " Average RSSI: " + String(tolRSSI/rxNumber) + " Max RSSI: " + String(maxRSSI) + " Min RSSI: " + String(minRSSI) + " Total Time on Air: " + String(totalTimeOnAir) + " Min Time on Air: " + String(minTimeOnAir) + " Max Time on Air: " + String(maxTimeOnAir));
+     // Your Domain name with URL path or IP address with path
+     http.begin(serverPath.c_str());
+     // If you need Node-RED/server authentication, insert user and password below
+     //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+     // Send HTTP GET request
+     int httpResponseCode = http.GET();
+     if (httpResponseCode>0) {
+       Serial.print("HTTP Response code: ");
+       Serial.println(httpResponseCode);
+       String payload = http.getString();
+       Serial.println(payload);
+       delay(1000);
+       txSheet = true;
+       rxNumber = 0;
+       tolRSSI = 0;
+       maxRSSI = -1000;
+       minRSSI = 1000;
+       totalTimeOnAir =0;
+       maxTimeOnAir = 0;
+       tolrxSize = 0;
+       minTimeOnAir=100000;
+       
+     }
+     else {
+       Serial.print("Error code: ");
+       Serial.println(httpResponseCode);
+     }
+     // Free resources
+     http.end();
+   }
+   else {
+     Serial.println("WiFi Disconnected");
+   }
+   
+ }
+
 
 
 void loop()
